76d01c997202e8c5ee8c79dbf6a64ba563d131f6
ryuya takei <maibadilin@gmail.com>
2021-09-26 17:27:53 +0900

    1-18行目新規作成

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,2 +1,18 @@
 class LinebotController < ApplicationController
-end
+    require 'line/bot' # gem 'line-bot-api'
+    require 'open-uri'
+    require 'kconv'
+    require 'rexml/document'
+
+    def callback
+        body = request.body.read
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            return head :bad_request
+            end
+            events = client.parse_events_from(body)
+            events.each { |event|
+             case event
+                # メッセージが送信された場合の対応(機能①)
+             when Line::Bot::Event::Message
+                    }
