a7c303fbbb5ce088e5cc2337437280174f18273a
take114514 <maibadilin@gmail.com>
2021-10-30 17:04:17 +0900

    新規作成

--- db/migrate/20210924060757_create_users.rb
+++ db/migrate/20210924060757_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :line_id, null: false #新規修　正
+      t.string :line_id, null: false 
 
       t.timestamps
     end
